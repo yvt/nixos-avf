25782f7eca6bc952d3faa0a66ff85d1985551465
yvt <i@yvt.jp>
2026-07-04 22:06:58 +0900

    remove ttyd client cert patch

--- avf/pkgs.nix
+++ avf/pkgs.nix
@@ -7,22 +7,11 @@
   libwebsockets,
 }:
 {
-  ttyd =
-    (ttyd.override {
-      libwebsockets = libwebsockets.overrideAttrs (
-        a:
-        lib.optionalAttrs (lib.strings.versionOlder a.version "4.3.6") {
-          patches = [
-            "${base}/build/debian/ttyd/client_cert.patch"
-          ];
-        }
-      );
-    }).overrideAttrs
-      (a: {
-        patches = [
-          "${base}/build/debian/ttyd/xtermjs_a11y.patch"
-        ];
-      });
+  ttyd = ttyd.overrideAttrs (a: {
+    patches = [
+      "${base}/build/debian/ttyd/xtermjs_a11y.patch"
+    ];
+  });
 
   android_virt = lib.recurseIntoAttrs {
     # TODO: linux_vm_manager
